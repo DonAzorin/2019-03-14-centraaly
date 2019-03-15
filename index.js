a98f97df13d657c8e2cfe9d3cd58862978ef7db0
MoisesAlVe <moalve7690@outlook.com>
2019-03-14 20:18:30 -0600

    añadido método GET para responder con un mensaje

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const express = require('exprees');
+const express = require('express');
+const app = express(); //Ejecuta express para acceder a sus métodos y funcionalidades
+const port = 8080;
+const chalk = require('chalk'); //Permite colorear los mensajes de la consola
+
+//Rutas
+app.get('/', (req,res) => {
+  res.send('estoy respondiendo algo :3');
+});
+
+//Escuchar peticiones en un puerto
+app.listen(port /*8080*/, () => {
+  console.log(`Escuchando en el puerto ${port}...`);
+  console.log(chalk.cyan(`http://localhost:${port}`));
+});
